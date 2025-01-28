52478184768ad1945ea5db9e8edb9bac275ab5df
shaikh-mohd-fahad <fahadazmilko@gmail.com>
2025-01-29 00:21:06 +0530

    test2

--- frontend/api/db.js
+++ frontend/api/db.js
@@ -3,13 +3,13 @@ import { promises as fs } from 'fs';
 
 export default async function handler(req, res) {
   try {
-    // Define the absolute path to the db.json file
+    // Resolve the absolute path of db.json
     const filePath = path.resolve('./api/db.json');
 
-    // Read the db.json file
+    // Read the file
     const data = await fs.readFile(filePath, 'utf-8');
 
-    // Parse and send the JSON data
+    // Send JSON response
     res.status(200).json(JSON.parse(data));
   } catch (error) {
     console.error('Error reading db.json:', error);
